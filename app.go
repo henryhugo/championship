c5099666def7e352eb5d023214eae06fa7e8870a
Hugo Henry <hugo.henry95@free.fr>
2018-11-15 21:22:30 +0100

    dep

--- app.go
+++ app.go
@@ -13,8 +13,10 @@ func champ(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-
+	//in memory strorage
 	leaguedb.Global_db = &leaguedb.LeaguesDB{}
+
+	//mongodb storage
 	leaguedb.Global_db.Init()
 
 	port := os.Getenv("PORT")
