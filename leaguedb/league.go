b2529ded476add4ed67124238717038d8997691c
Hugo Henry <hugo.henry95@free.fr>
2018-11-15 21:34:50 +0100

    test /league

--- leaguedb/league.go
+++ leaguedb/league.go
@@ -1,8 +1,13 @@
 package leaguedb
 
+import (
+	"net/http"
+)
+
 type LeaguesStorage interface {
 	Init()
 	Add(l League) error
+	leagueHandler(w http.ResponseWriter, r *http.Request)
 }
 
 type League struct {
@@ -23,3 +28,25 @@ func (db *LeaguesDB) Add(l League) error {
 	db.leagues[l.LeagueID] = l
 	return nil
 }
+
+/*func leagueHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		var l League
+		err := json.NewDecoder(r.Body).Decode(&l)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		Global_db.Add(l)
+		fmt.Fprint(w, "ok") // 200 by default
+		return
+
+	case "GET":
+		fmt.Fprintln(w, "not implemented yet")
+
+	default:
+
+		fmt.Fprintln(w, "not implemented yet !")
+	}
+}*/
